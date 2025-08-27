c3536bd81b4923add4f36894c70853e9b386abfa
luiscabezasdev <luiscabezas25@hotmail.com>
2025-08-26 20:52:13 -0500

    nuevo commit

--- JavaApplication2/src/JavaApplication2.java
+++ JavaApplication2/src/JavaApplication2.java
@@ -10,16 +10,16 @@ public class JavaApplication2 {
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Ingrese un número entero X: ");
-        
+        5
         int X = sc.nextInt();
         
         double Y;
         
         if (X < 0) {
             Y = 100 * X;
-            } else if (X >= 0 && X < 3) {   // se cumple 0 <= X < 3
+            } else if (X >= 0 && X < 3) {   
                 Y = 2 * X + 5;
-            } else if (X >= 3 && X <= 6) {  // se cumple 3 <= X <= 6
+            } else if (X >= 3 && X <= 6) {  
                 Y = 100.0 / X;
             } else {
                 Y = 0;
